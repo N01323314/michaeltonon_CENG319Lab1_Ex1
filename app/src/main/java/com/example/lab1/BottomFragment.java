ab3b4e8f645125ea6fda1657d669e59455e164fd
Michael Tonon <mikie@Michael-PC.hitronhub.home>
2020-09-22 12:37:56 -0400

    Initial commit

---
+++ app/src/main/java/com/example/lab1/BottomFragment.java
@@ -0,0 +1,76 @@
+package com.example.lab1;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class BottomFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_bottom, container, false);
+        return view;
+    }
+
+    public void displayCreate (boolean a){
+        TextView textView = getActivity().findViewById(R.id.cyclesactive);
+        String msg = getString(R.string.create);
+        if (a){
+            textView.setText(msg);
+        }
+    }
+
+    public void displayStart (boolean a){
+        TextView textView = getActivity().findViewById(R.id.cyclesactive2);
+        String msg = getString(R.string.start);
+        if (a){
+            textView.append(msg + "\n");
+        }
+    }
+
+    public void displayPause (boolean a){
+        String msg = getString(R.string.pause);
+        TextView textView = getActivity().findViewById(R.id.cyclesactive2);
+        if (a){
+            textView.setText(msg + "\n");
+            textView = getActivity().findViewById(R.id.cyclesactive);
+            textView.setVisibility(View.GONE);
+        }
+    }
+
+    public void displayRestart (boolean a){
+        String msg = getString(R.string.restart);
+        TextView textView = getActivity().findViewById(R.id.cyclesactive2);
+        if (a){
+            textView.setText(msg + "\n");
+        }
+    }
+
+    public void displayResume (boolean a){
+        String msg = getString(R.string.resume);
+        TextView textView = getActivity().findViewById(R.id.cyclesactive2);
+        if (a){
+            textView.append(msg + "\n");
+        }
+    }
+
+    public void displayStop (boolean a){
+        String msg = getString(R.string.stop);
+        TextView textView = getActivity().findViewById(R.id.cyclesactive2);
+        if (a){
+            textView.append(msg + "\n");
+        }
+    }
+
+    public void displayDestroy (boolean a){
+        String msg = getString(R.string.destroy);
+        TextView textView = getActivity().findViewById(R.id.cyclesactive2);
+        if (a){
+            textView.append(msg + "\n");
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    String tag = "Lifecycle";
+    private BottomFragment fragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fragment = (BottomFragment)getSupportFragmentManager().findFragmentById(R.id.bottomfragment);
+        boolean check = true;
+        fragment.displayCreate(check);
+        Log.d(tag, "In the onCreate() event");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        fragment = (BottomFragment)getSupportFragmentManager().findFragmentById(R.id.bottomfragment);
+        boolean check = true;
+        fragment.displayStart(check);
+        Log.d(tag, "In the onStart() event");
+    }
+
+    @Override
+    public void onRestart() {
+        super.onRestart();
+        fragment = (BottomFragment)getSupportFragmentManager().findFragmentById(R.id.bottomfragment);
+        boolean check = true;
+        fragment.displayRestart(check);
+        Log.d(tag, "In the onRestart() event");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        fragment = (BottomFragment)getSupportFragmentManager().findFragmentById(R.id.bottomfragment);
+        boolean check = true;
+        fragment.displayResume(check);
+        Log.d(tag, "In the onResume() event");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        fragment = (BottomFragment)getSupportFragmentManager().findFragmentById(R.id.bottomfragment);
+        boolean check = true;
+        fragment.displayPause(check);
+        Log.d(tag, "In the onPause() event");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        fragment = (BottomFragment)getSupportFragmentManager().findFragmentById(R.id.bottomfragment);
+        boolean check = true;
+        fragment.displayStop(check);
+        Log.d(tag, "In the onStop() event");
+    }
+
+    @Override
+    public void onDestroy() {
+        fragment = (BottomFragment)getSupportFragmentManager().findFragmentById(R.id.bottomfragment);
+        boolean check = true;
+        fragment.displayDestroy(check);
+        super.onDestroy();
+        Log.d(tag, "In the onDestroy() event");
+    }
+
+
+
+}
